cb1fd54b7236ad9f4bd3357348a5099293f9e272
ManarMajeed <imavx3@gmail.com>
2023-11-27 11:27:32 +0300

    Adding LostItemReport class Manar

---
+++ lib/lost_item_report.dart
@@ -0,0 +1,10 @@
+class LostItemReport {
+  String? category;
+  String? lostDate;
+  String? expectedPlace;
+  String? desription;
+  LostItemReport(category, lostDate, expectedPlace, desription) {
+    // ignore: avoid_print
+    print("object");
+  }
+}
