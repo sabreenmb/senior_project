1cfea82a7a49c9d2811265d05c78612b2c42296b
ManarMajeed <imavx3@gmail.com>
2023-11-28 21:53:34 +0300

    add import

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:senior_project/interface/LoginScreen.dart';
+import 'package:senior_project/interface/ServicesScreen.dart';
 import 'package:senior_project/interface/LostAndFoundScreen.dart';
 
 import 'firebase_options.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
       ],
       supportedLocales: [Locale('ar', 'AE')],
       locale: Locale("ar", "AE"),
-      home: LostAndFoundScreen(),
+      home: ServicesScreen(),
     );
   }
 }
