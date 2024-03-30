659ce8186c8039bd0108e2fe8594b004af758f4c
Maya-mohammed <mayamkr1@gmail.com>
2024-03-30 07:32:02 +0300

    MayaLastv

--- lib/interface/SaveListScreen.dart
+++ lib/interface/SaveListScreen.dart
@@ -1,20 +1,196 @@
 import 'package:flutter/material.dart';
-import 'package:senior_project/interface/lost_and_found_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:senior_project/constant.dart';
+import 'package:senior_project/interface/ChatScreen.dart';
+import 'package:senior_project/interface/HomeScreen.dart';
+import 'package:senior_project/interface/add_lost_item_screen.dart';
+//import 'package:senior_project/interface/create_group.dart';
+import 'package:senior_project/interface/services_screen.dart';
+import 'package:senior_project/theme.dart';
+import 'package:senior_project/widgets/side_menu.dart';
 
-class SaveListScreen extends StatelessWidget {
+class SaveListScreen extends StatefulWidget {
   const SaveListScreen({super.key});
 
   @override
+  State<SaveListScreen> createState() => _SaveState();
+}
+
+class _SaveState extends State<SaveListScreen>
+    with SingleTickerProviderStateMixin {
+  late List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 3;
+  //search
+  ////List<VolunteerOpReport> searchSessionList = [];
+
+  // List<VolunteerOpReport> _volunteerOpReport = [];
+  //create button
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      {
+        'page': const HomeScreen(),
+      },
+      {
+        'page': const ChatScreen(),
+      },
+      {
+        'page': const AddLostItemScreen(),
+      },
+      {
+        'page': const ServisesScreen(),
+      },
+      {
+        'page': const SaveListScreen(),
+      },
+    ];
+    _LoadCreatedSessions();
+  }
+
+  void _LoadCreatedSessions() async {}
+
+  void _selectPage(int index) {
+    setState(() {
+      if (index == 1) {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => const ServisesScreen()));
+        _selectedPageIndex = index;
+      }
+      //todo uncomment on next sprints
+      // if (index == 0) {
+      //   Navigator.pushReplacement(
+      //       context, MaterialPageRoute(builder: (_) => HomeScreen()));
+      // } else if (index == 1) {
+      //   Navigator.pushReplacement(
+      //       context, MaterialPageRoute(builder: (_) => ServisesScreen()));
+      // } else if (index == 2) {
+      //   Navigator.pushReplacement(
+      //       context, MaterialPageRoute(builder: (_) => ChatScreen()));
+      // } else if (index == 3) {
+      //   Navigator.pushReplacement(
+      //       context, MaterialPageRoute(builder: (_) => SaveListScreen()));
+      // }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print('build enter');
+
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (_) => const LostAndFoundScreen()));
-          },
-          child: const Text('back'),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color.fromARGB(255, 239, 237, 237),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: CustomColors.pink,
+        elevation: 0,
+        title: Text(" المحفوظات", style: TextStyles.heading1),
+        centerTitle: false,
+        iconTheme: const IconThemeData(color: CustomColors.darkGrey),
+      ),
+      endDrawer: const SideDrawer(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 0.1,
+        clipBehavior: Clip.none,
+        child: SizedBox(
+          height: kBottomNavigationBarHeight * 1.2,
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: BottomNavigationBar(
+              onTap: _selectPage,
+              unselectedItemColor: CustomColors.darkGrey,
+              selectedItemColor: CustomColors.darkGrey,
+              currentIndex: _selectedPageIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  label: 'الرئيسية',
+                  icon: Icon(Icons.home_outlined),
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.apps), label: 'الخدمات'),
+                // BottomNavigationBarItem(
+                //   label: "",
+                //   activeIcon: null,
+                //   icon: Icon(null),
+                // ),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.messenger_outline,
+                    ),
+                    label: 'الدردشة'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.bookmark_border), label: 'المحفوظات'),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ModalProgressHUD(
+        color: Colors.black,
+        opacity: 0.5,
+        progressIndicator: CircularProgressIndicator(),
+        inAsyncCall: isLoading,
+        child: SafeArea(
+          bottom: false,
+          child: Column(
+            children: [
+              const SizedBox(height: 15),
+              Expanded(
+                child: ListView(
+                  children: [
+                    _buildTextCard("كرة سلة", "2024-3-06"),
+                    _buildTextCard("مناقشة كتاب", "2024-3-08"),
+                    _buildTextCard("احصاء عام", "2024-3-26"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextCard(String title, String date) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: CustomColors.lightBlue, width: 2),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        onTap: () {
+          print('Card tapped');
+        },
+        child: Container(
+          height: 100,
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(title,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColors.pink)),
+              SizedBox(height: 10),
+              Text(date,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: CustomColors.darkGrey,
+                  )),
+            ],
+          ),
         ),
       ),
     );
