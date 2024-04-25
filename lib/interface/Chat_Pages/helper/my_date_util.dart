faafa2c191abc4a9ed665608eb7d49872b5c0f81
ManarMajeed <imavx3@gmail.com>
2024-04-25 12:28:11 +0300

    enhancment manar
    Signed-off-by: ManarMajeed <imavx3@gmail.com>

--- lib/interface/Chat_Pages/helper/my_date_util.dart
+++ lib/interface/Chat_Pages/helper/my_date_util.dart
@@ -8,24 +8,6 @@ class MyDateUtil {
     return TimeOfDay.fromDateTime(date).format(context);
   }
 
-  // for getting formatted time for sent & read
-  static String getMessageTime(
-      {required BuildContext context, required String time}) {
-    final DateTime sent = DateTime.fromMillisecondsSinceEpoch(int.parse(time));
-    final DateTime now = DateTime.now();
-
-    final formattedTime = TimeOfDay.fromDateTime(sent).format(context);
-    if (now.day == sent.day &&
-        now.month == sent.month &&
-        now.year == sent.year) {
-      return formattedTime;
-    }
-
-    return now.year == sent.year
-        ? '$formattedTime - ${sent.day} ${_getMonth(sent)}'
-        : '$formattedTime - ${sent.day} ${_getMonth(sent)} ${sent.year}';
-  }
-
   //get last message time (used in chat user card)
   static String getLastMessageTime(
       {required BuildContext context,
@@ -44,33 +26,6 @@ class MyDateUtil {
         ? '${sent.day} ${_getMonth(sent)} ${sent.year}'
         : '${sent.day} ${_getMonth(sent)}';
   }
-
-  //get formatted last active time of user in chat screen
-  // static String getLastActiveTime(
-  //     {required BuildContext context, required String lastActive}) {
-  //   final int i = int.tryParse(lastActive) ?? -1;
-
-  //   //if time is not available then return below statement
-  //   if (i == -1) return 'Last seen not available';
-
-  //   DateTime time = DateTime.fromMillisecondsSinceEpoch(i);
-  //   DateTime now = DateTime.now();
-
-  //   String formattedTime = TimeOfDay.fromDateTime(time).format(context);
-  //   if (time.day == now.day &&
-  //       time.month == now.month &&
-  //       time.year == time.year) {
-  //     return 'Last seen today at $formattedTime';
-  //   }
-
-  //   if ((now.difference(time).inHours / 24).round() == 1) {
-  //     return 'Last seen yesterday at $formattedTime';
-  //   }
-
-  //   String month = _getMonth(time);
-
-  //   return 'Last seen on ${time.day} $month on $formattedTime';
-  // }
 
   // get month name from month no. or index
   static String _getMonth(DateTime date) {
